00fe512241f8e7cf108a36ec1d725f27c89370dd
David Ruyle <djr.taureau@gmail.com>
2019-10-20 14:47:13 -0500

    docker deploy

--- src/app/services/seo.service.ts
+++ src/app/services/seo.service.ts
@@ -1,26 +1,31 @@
-import { Injectable } from '@angular/core';
-import { Title, Meta } from '@angular/platform-browser';
-import { Router } from '@angular/router';
+import { Injectable } from "@angular/core";
+import { Title, Meta } from "@angular/platform-browser";
+import { Router } from "@angular/router";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class SeoService {
+  constructor(
+    private title: Title,
+    private meta: Meta,
+    private router: Router
+  ) {}
 
-  constructor(private title: Title, private meta: Meta, private router: Router) { }
-
-  generateTags({ title = '', description = '', image = '' }) {
-
+  generateTags({ title = "", description = "", image = "" }) {
     this.title.setTitle(title);
     this.meta.addTags([
       // Open Graph
-      { name: 'og:url', content: `https://firestarter.fireship.io${this.router.url}` },
-      { name: 'og:title', content: title },
-      { name: 'og:description', content: description },
-      { name: 'og:image', content: image },
+      {
+        name: "og:url",
+        content: `https://localhost:8080${this.router.url}`
+      },
+      { name: "og:title", content: title },
+      { name: "og:description", content: description },
+      { name: "og:image", content: image },
       // Twitter Card
-      { name: 'twitter:card', content: 'summary' },
-      { name: 'twitter:site', content: '@fireship_dev' },
+      { name: "twitter:card", content: "summary" },
+      { name: "twitter:site", content: "@druyle" }
     ]);
   }
 }
